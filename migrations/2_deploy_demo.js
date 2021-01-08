fbb9457dd878cf17edc7cff0afed2e31d2c16d0c
Codegnosis <paul@codegnosis.co.uk>
2021-01-08 12:12:36 +0000

    migrations - update with new demo contract

--- migrations/2_deploy_demo.js
+++ migrations/2_deploy_demo.js
@@ -1,15 +1,9 @@
 require("dotenv").config()
 const DemoConsumer = artifacts.require("DemoConsumer")
-const DemoConsumerCustom = artifacts.require("DemoConsumerCustom")
 
-const {
-  CONSUMER_LIB_ADDRESS,
-  ROUTER_ADDRESS,
-} = process.env
+const { CONSUMER_LIB_ADDRESS, ROUTER_ADDRESS } = process.env
 
 module.exports = function(deployer) {
   DemoConsumer.link("ConsumerLib", CONSUMER_LIB_ADDRESS)
-  DemoConsumerCustom.link("DemoConsumerCustom", CONSUMER_LIB_ADDRESS)
   deployer.deploy(DemoConsumer, ROUTER_ADDRESS)
-  deployer.deploy(DemoConsumerCustom, ROUTER_ADDRESS)
 }
